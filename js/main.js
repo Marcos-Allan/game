2e723a9ac50481f40a9302fa3d8a4dd3ad2b6425
Marcos Allan <allanmenezes880@gmail.com>
2023-09-10 09:47:39 -0300

    adição de novo controle de jogo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const choices = [...document.querySelectorAll('section')]
+const divGame = document.querySelector('#game')
+let yo = undefined
+let maq = undefined
+
 
 function choiceGame(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a
@@ -6,35 +10,52 @@ function choiceGame(a, b) {
 
 choices.map((el, i) => {
     el.addEventListener('click', () => {
-        console.log(el.innerText)
-        console.log(i)
         calcularResultado(i, choiceGame(0, 2))
-        let cc = undefined
-        if(choiceGame(0, 2) == 0 ){
-            cc = 'Pedra'
-        }else if(choiceGame(0, 2) == 1){
-            cc = 'Papel'
-        }else if(choiceGame(0, 2) == 2){
-            cc = 'Tesoura'
-        }
-        console.log('VOCÊ: '+el.innerText+' JOGO: '+cc)
     })
 })
 
+document.addEventListener('keydown', (event) => {
+    if((event.key == 1)||(event.key == 2)||(event.key == 3)){
+        calcularResultado(event.key - 1, choiceGame(0, 2))
+    }
+})
+
 function calcularResultado(p1, p2) {
+    
+    // VER O QUE O JOGADOR E A MAQUINA JOGOU
+    if(p1 == 0){
+        yo = 'Pedra'
+    }
+    else if(p1 == 1){
+        yo = 'Papel'
+    }else if(p1 == 2){
+        yo = 'Tesoura'
+    }
+
+    if(p2 == 0){
+        maq = 'Pedra'
+    }
+    else if(p2 == 1){
+        maq = 'Papel'
+    }else if(p2 == 2){
+        maq = 'Tesoura'
+    }
+
     // GAME OVER
     if((p1 == 0 && p2 == 1)||(p1 == 1 && p2 == 2)||(p1 == 2 && p2 == 0)){
-        alert('perdeu')
+        divGame.innerHTML = '<p>Voce: '+yo+'<br>maquina: '+maq+'<br>Derrota</p>'
     }
 
     // EMPATE
     else if((p1 == 0 && p2 == 2)||(p1 == 1 && p2 == 0)||(p1 == 2 && p1 == 1 )){
-        alert('ganhou')
+        divGame.innerHTML = '<p>Voce: '+yo+'<br>maquina: '+maq+'<br>Vitória</p>'
     }
 
     // VITÓRIA
     else if((p1 == 0 && p2 ==0)||(p1 == 1 && p1 == 1)||(p2 == 2 && p2 == 2)){
-        alert('empate')
+        divGame.innerHTML = '<p>Voce: '+yo+'<br>maquina: '+maq+'<br>Empate</p>'
     }
+
+    // alert('VOCÊ: '+yo+' JOGO: '+maq)
 }
 // const choiceJogador
